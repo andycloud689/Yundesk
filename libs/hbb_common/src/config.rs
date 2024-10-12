db8e7237cadebb370a3618c844f8f0d9b7c294d0
andycloud689 <andycloud689@gmail.com>
2024-10-12 16:38:58 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.frp.ibyt.fun"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk.ibyt.fun"];
 pub const PUBLIC_RS_PUB_KEY: &str = "6Ab15cJR7RB4KvqjCTpRpRQLVCzAncexxJ0sDxRoi2k=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
